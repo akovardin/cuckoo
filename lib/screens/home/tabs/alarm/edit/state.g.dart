f62d7d1e9028189b3d37ad5add5539b4fff3a229
horechek <horechek@gmail.com>
2021-12-26 11:21:58 +0300

    reformat states

---
+++ lib/screens/home/tabs/alarm/edit/state.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// CopyWithGenerator
+// **************************************************************************
+
+extension AlarmStateCopyWith on AlarmState {
+  AlarmState copyWith({
+    AlarmModel? alarm,
+  }) {
+    return AlarmState(
+      alarm: alarm ?? this.alarm,
+    );
+  }
+}
---
+++ lib/screens/home/tabs/alarm/list/state.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// CopyWithGenerator
+// **************************************************************************
+
+extension ListStateCopyWith on ListState {
+  ListState copyWith({
+    bool? edit,
+    List<AlarmModel>? list,
+  }) {
+    return ListState(
+      edit: edit ?? this.edit,
+      list: list ?? this.list,
+    );
+  }
+}
